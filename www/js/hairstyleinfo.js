bfe0327598cc5d8023bb133badeba2c2bb6f966c
Admin <admin@Administrator.local>
2019-02-13 19:17:12 +1000

    use promises

--- www/js/hairstyleinfo.js
+++ www/js/hairstyleinfo.js
@@ -7,3 +7,12 @@ function HairStyleInfo(style, title, description) {
     this.image2 = '/' + style + '/' + style + '2.jpg';
     this.image3 = '/' + style + '/' + style + '3.jpg';
 }
+function HairStyleInfo(data) {
+    this.style = data.style;
+    this.title = 'The ' + data.title;
+    this.description = data.description;
+    this.mainImage = '/' + data.style + '/' + data.style + 'main.jpg';
+    this.image1 = '/' + data.style + '/' + data.style + '1.jpg';
+    this.image2 = '/' + data.style + '/' + data.style + '2.jpg';
+    this.image3 = '/' + data.style + '/' + data.style + '3.jpg';
+}
--- www/js/main.js
+++ www/js/main.js
@@ -1,34 +1,4 @@
-var asymmetrical = new HairStyleInfo('asymmetrical', 'Asymmetrical', '');
-var businessman = new HairStyleInfo('businessman', 'Buiness Man', '');
-var buzzcut = new HairStyleInfo('buzzcut', 'Buzz Cut', '');
-var caesar = new HairStyleInfo('caesar', 'Caesar', '');
-var crewcut = new HairStyleInfo('crewcut', 'Crew Cut', '');
-var fauxhawk = new HairStyleInfo('fauxhawk', 'Faux Hawk', '');
-var highandtight = new HairStyleInfo('highandtight', 'High and Tight', '');
-var mohawk = new HairStyleInfo('mohawk', 'Mohawk', '');
-var pompadour = new HairStyleInfo('pompadour', 'Pompadour', '');
-var quiff = new HairStyleInfo('quiff', 'Quiff', '');
-var skinfade = new HairStyleInfo('skinfade', 'Skin Fade', '');
-var slickedback = new HairStyleInfo('slickedback', 'Slicked Back', '');
-var spikyhair = new HairStyleInfo('spikyhair', 'Spiky Hair', '');
-var undercut = new HairStyleInfo('undercut', 'Undercut', '');
-var hairstyles =  { asymmetrical: asymmetrical,
-                    businessman: businessman,
-                    buzzcut: buzzcut,
-                    caesar: caesar,
-                    crewcut: crewcut,
-                    fauxhawk: fauxhawk,
-                    highandtight: highandtight,
-                    mohawk: mohawk,
-                    pompadour: pompadour,
-                    quiff: quiff,
-                    skinfade: skinfade,
-                    slickedback: slickedback,
-                    spikyhair: spikyhair,
-                    undercut: undercut }
-
-                    /*
-var navmenulist = document.getElementById('navmenulist');
+/*
 var newListItem = document.createElement('li');
 var newListItemLink = document.createElement('a');
 newListItemLink.setAttribute('href', '/?s=' + 'crewcut');
@@ -37,23 +7,30 @@ newListItem.appendChild(newListItemLink);
 navmenulist.appendChild(newListItem);
 */
 
-Object.keys(hairstyles).forEach(function(style) {
-    $.get('/' + style + '/info', function(data) {
-        hairstyles[style].description = data;
-    }, 'text');
-})
 
 $(function() {
+    var styles = ['buzzcut', 'caesar'];
+    Promise.all(styles.map(style => fetch('/' + style + '/info.json'))).then(responses =>
+    Promise.all(responses.map(res => res.json()))).then(data => {  //.json() returns another promise!
+        console.log(data);
+        console.log('end');
+    }).catch(err => console.error(err));
+
     let s = window.location.search;
     let styleString = s.substring(3);
-    let style = hairstyles[styleString];
-    if (styleString != "" & style != undefined) {
-        document.title = style.title;
-        document.getElementsByClassName('title')[0].innerHTML = style.title;
-        document.getElementsByClassName('description')[0].innerHTML = style.description;
-        document.getElementsByClassName('mainImage')[0].src = style.mainImage;;
-        document.getElementsByClassName('image1')[0].src = style.image1;
-        document.getElementsByClassName('image2')[0].src = style.image2;
-        document.getElementsByClassName('image3')[0].src = style.image3;
-    }
+    if (styleString == "" || styleString == undefined)
+        return console.error('Style Not Found!');
+    fetch('/' + styleString + '/info.json')
+        .then(response => response.json())
+        .then(data => {
+            let style = new HairStyleInfo(data);
+            document.title = style.title;
+            document.getElementsByClassName('title')[0].innerHTML = style.title;
+            document.getElementsByClassName('description')[0].innerHTML = style.description;
+            document.getElementsByClassName('mainImage')[0].src = style.mainImage;;
+            document.getElementsByClassName('image1')[0].src = style.image1;
+            document.getElementsByClassName('image2')[0].src = style.image2;
+            document.getElementsByClassName('image3')[0].src = style.image3;
+        }).catch(err => console.error(err));
+
 });
