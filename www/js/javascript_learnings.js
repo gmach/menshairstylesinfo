00f637ec4c1e0f2bb6e643d3b063b6046284cb4a
Admin <admin@Administrator.lan>
2019-03-12 15:26:19 +1000

    a

--- www/js/javascript_learnings.js
+++ www/js/javascript_learnings.js
@@ -76,3 +76,15 @@ async function go() {
 }
 
 go();
+
+// Function to lazy load a script
+function loadScript(src) {
+    return new Promise(function(resolve, reject) {
+        const script = document.createElement('script');
+        script.async = true;
+        script.src = src;
+        script.onload = resolve;
+        script.onerror = reject;
+        document.head.appendChild(script);
+    });
+}
